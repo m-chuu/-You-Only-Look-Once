5f695a0b7a8b8d4a838ba42dab6b5752902de764
matt <matthewtan1217@gmail.com>
2026-07-26 16:00:52 +0800

    YOLO with RGB and Sober

--- 98_YOLOv3_ori/02_Run_YOLO_Model.py
+++ 98_YOLOv3_ori/02_Run_YOLO_Model.py
@@ -1,8 +1,12 @@
+import os
 from ultralytics import YOLO
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 model = YOLO("best.pt")   # get the best.pt from your  train result 
 results = model("./01_Train_images/test/images/", #image file directory #input image
-                save=True, project="Inference", name="detection_result") #output image
+                save=True, project=os.path.join(script_dir, "runs/detect/Inference"), name="detection_result") #output image
 
 print("Inference completed")
 
